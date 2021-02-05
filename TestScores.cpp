6fb1c4c9aa7d933f534312682dba924421ae08e1
William Coughlin <coughlinw97@gmail.com>
2021-02-05 00:50:48 -0500

    Add files via upload

--- TestScores.cpp
+++ TestScores.cpp
@@ -2,10 +2,11 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
-void SortArrAscending(double*, const int);
+void SortArrAscending(double*, std::string*, const int);
 double Average(double*, const int);
-void DisplayArr(const double*, const int);
+void DisplayArr(const double*, const std::string*, const int);
 
 int main() {
 	int numScores;
@@ -14,11 +15,17 @@ int main() {
 	std::cin >> numScores;
 
 	double* scoresArr = new double[numScores];
+	std::string* namesArr = new std::string[numScores];
 
 	for (int index = 0; index < numScores; index++) {
-		double score = 0;
+		int score;
+		std::string student;
 
-		std::cout << "\nEnter test score #" << index + 1 << ": ";
+		std::cout << "\nEnter name of student for test #" << index + 1 << ": ";
+		std::cin >> student;
+		*(namesArr + index) = student;
+
+		std::cout << "\nEnter score for test #" << index + 1 << ": ";
 		std::cin >> score;
 
 		while (score < 0) {
@@ -30,19 +37,20 @@ int main() {
 		*(scoresArr + index) = score;
 	}
 
-	SortArrAscending(scoresArr, numScores);
+	SortArrAscending(scoresArr, namesArr, numScores);
 
-	std::cout << std::setprecision(5) << "\nThe average test score was " << Average(scoresArr, numScores);
+	std::cout << std::setprecision(4) << "\nThe average test score was " << Average(scoresArr, numScores) << std::endl;
 
-	DisplayArr(scoresArr, numScores);
+	DisplayArr(scoresArr, namesArr, numScores);
 }
 
 
-void SortArrAscending(double* arr, const int size) {
+void SortArrAscending(double* scoreArr, std::string* nameArr, const int size) {
 	for (int max = size - 1; max > 0; max--) {
 		for (int index = 0; index < max; index++) {
-			if (*(arr + index) > *(arr + (index + 1))) {
-				std::swap(*(arr + index), *(arr + (index + 1)));
+			if (*(scoreArr + index) > *(scoreArr + (index + 1))) {
+				std::swap(*(scoreArr + index), *(scoreArr + (index + 1)));
+				std::swap(*(nameArr + index), *(nameArr + (index + 1)));
 			}
 		}
 	}
@@ -61,8 +69,8 @@ double Average(double* arr, const int num) {
 	return average;
 }
 
-void DisplayArr(const double* arr, const int size) {
+void DisplayArr(const double* scoreArr, const std::string* nameArr, const int size) {
 	for (int index = 0; index < size; index++) {
-		std::cout << "\nTest Score #" << index + 1 << ": " << *(arr + index) << std::endl;
+		std::cout << "\nTest Score #" << index + 1 << ": " << *(nameArr + index) << " " << *(scoreArr + index) << std::endl;
 	}
 }
